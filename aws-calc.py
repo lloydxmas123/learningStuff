1ed8d7a6fb7aac8475e0e0e82b6973e0acda9468
lloydxmas123 <lloydcisco@gmail.com>
2018-03-15 16:57:47 -0400

    modified aws-calc.py

--- aws-calc.py
+++ aws-calc.py
@@ -4,4 +4,4 @@ def annualCost():
     ec2 = float(input("Hourly EC2 Cost: "))
     lic = float(input("Hourly License Cost: "))
     cost = (ec2 * 8760) + (lic * 8760)
-    print("Your EC2 instance will cost ${} per year. ")
+    print("Your EC2 instance will cost ${} per year. ").format(cost)
